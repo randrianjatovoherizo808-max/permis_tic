edd2cf3835c80989783eeea0d3ed9fafb1569f16
Herizo RANDRIANJATOVO <randrianjatovoherizo808@gmail.com>
2026-06-27 11:42:17 +0300

    fix: asgi.py - retirer prefix backend

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -11,5 +11,5 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ.get('DJANGO_SETTINGS_MODULE', 'backend.config.settings'))
 application = get_asgi_application()
